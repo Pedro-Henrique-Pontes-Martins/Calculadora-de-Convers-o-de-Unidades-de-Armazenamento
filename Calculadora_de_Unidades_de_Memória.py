0a90dc5535ed252ad1a45fb93fe10fb98a517a40
unknown <pedrosenaitech@gmail.com>
2023-03-23 09:52:57 -0300

    Criação da função para a converção de string para float e inserçãodo input

---
+++ Calculadora_de_Unidades_de_Memória.py
@@ -0,0 +1,8 @@
+Const_ValorParaConverção = 1024
+
+def converterStringParaFloat(value):
+    return float(value)
+
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertida  = converterStringParaFloat(input())
