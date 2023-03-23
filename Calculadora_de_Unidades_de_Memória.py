88f6e54400c58c881c88f0ba78e3ab7b05ce8d4b
unknown <pedrosenaitech@gmail.com>
2023-03-23 10:23:33 -0300

    Criação da função para a converção de bit para byte

--- Calculadora_de_Unidades_de_Memória.py
+++ Calculadora_de_Unidades_de_Memória.py
@@ -3,6 +3,12 @@ Const_ValorParaConverção = 1024
 def converterStringParaFloat(value):
     return float(value)
 
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    bytesCalculado = valorASerConvertido / 8
+    return bytesCalculado
 
 print('Insira o valor a ser convertido')
 entradaDoTecladoValorASerConvertida  = converterStringParaFloat(input())
+valorConvertido = bitParaByte(entradaDoTecladoValorASerConvertida)
+print(valorConvertido)
